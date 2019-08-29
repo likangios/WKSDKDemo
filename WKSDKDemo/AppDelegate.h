868e3d39ed7e52c5eba9ac40db7536edbef6b9c7
luck <likangios@163.com>
2019-08-30 01:12:32 +0800

    no message

---
+++ WKSDKDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WKSDKDemo
+//
+//  Created by luck on 2019/8/21.
+//  Copyright © 2019年 ting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WKSDKDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WKSDKDemo
+//
+//  Created by luck on 2019/8/21.
+//  Copyright © 2019年 ting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
